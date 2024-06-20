a099a267e3899a3e006e9a78f5dee0b7f74ca292
frumh002 <mateo.frumholtz127@gmail.com>
2024-06-20 13:33:04 -0500

    Import datasets and create total enrollment table.

---
+++ R/Import NCES.R
@@ -0,0 +1,6 @@
+# Install Needed Packages -------------------------------------------------
+
+library(readxl)
+
+# Import Datasets ---------------------------------------------------------
+df <- read_excel("/Users/mateofrumholtz/Dropbox/R Base/Projects/School Segregation/Data/MN_Stanford_NCES_data 91-22.xlsx", sheet = "LISDdata")
